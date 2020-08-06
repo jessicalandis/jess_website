774173f3dae7119e3e12409edf7c28a8c352b5e0
jess landis <jll285@cornell.edu>
2020-08-06 19:11:55 -0400

    github link

--- src/pages/Coding.js
+++ src/pages/Coding.js
@@ -6,7 +6,11 @@ export default class Coding extends Component {
     return (
       <div className="coding-container">
         <div className="coding-div">
-          <h2>This page is currently under construction...</h2>
+          <h2>
+            <a href="https://www.github.com/jessicalandis">
+              Visit my Github Here
+            </a>
+          </h2>
           <img className="codingpic" src={logo} alt="image " />
         </div>
       </div>
